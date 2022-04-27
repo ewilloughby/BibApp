577d652e4f657d641b8f51c2058b60f73f538c06
ewilloughby <willo1ej@gmail.com>
2022-04-27 08:41:59 -0400

    Update dev environment for CSRF

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # CSRF
+  config.action_controller.per_form_csrf_tokens = true
+  config.action_controller.forgery_protection_origin_check = true
 end
