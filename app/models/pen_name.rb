433d118e4c18039b1f099dca2de565b73f4f86e4
Eric Willoughby <willouge@mskcc.org>
2022-05-16 16:47:32 -0400

    Fix deprecated method causing error adding pen_names

--- app/models/pen_name.rb
+++ app/models/pen_name.rb
@@ -15,15 +15,21 @@ class PenName < ActiveRecord::Base
   scope :for_name_string, lambda { |name_string_id| where(:name_string_id => name_string_id) }
 
   def set_contributorships
+    logger.debug("\n\n ========== SET CONTRIBUTORSHIPS in PenName ========== \n\n")
     self.name_string.work_name_strings.each do |wns|
       #only create Contributorship for "accepted" works
+      
+      logger.debug(wns.inspect)
+      
       if wns.work and wns.work.accepted?
-        self.contributorships.find_or_create_by_work_id_and_person_id_and_role(wns.work.id, self.person_id, wns.role)
+        #self.contributorships.find_or_create_by_work_id_and_person_id_and_role(wns.work.id, self.person_id, wns.role)
+        self.contributorships.find_or_create_by(work_id: wns.work.id, person_id: self.person_id, role: wns.role)
       end
     end
   end
 
   def index_works
+    logger.debug("\n\n ========== INDEX WORKS in PenName ========== \n\n")
     self.works.each { |w| w.set_for_index_and_save }
     Index.batch_index
   end
