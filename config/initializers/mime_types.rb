1525dff84e2ddbe8b450af52bf40a82c0c8db4a6
Eric Willoughby <willouge@mskcc.org>
2023-05-01 13:46:39 -0400

    Ran rails app:update for 6.1

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,11 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register_alias "text/html", :iphone
 
-#### Register METS as XML MIME Type, so we can provide RESTful METS ####
-Mime::Type.register_alias "text/xml", :mets
-
-#### Register RDF as XML MIME Type, so we can provide RESTful RDF ####
-Mime::Type.register_alias "text/xml", :rdf
-
+Mime::Type.register "application/rdf", :rdf
+Mime::Type.register "application/mets", :mets
+Mime::Type.register 'application/x-endnote-refer', :ris
