72f97759a2eae568264fc0377dd12258c4f0faf4
Eric Willoughby <willouge@mskcc.org>
2023-05-04 00:15:12 -0400

    Update deploy and build for intel procs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock "~> 3.17.2"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.7.6'
-set :passenger_environment_variables, { rbenv_version: '2.7.6' }
+set :rbenv_ruby, '3.0.6'
+set :passenger_environment_variables, { rbenv_version: '3.0.6' }
 
 set :application, "bibapp"
 set :repo_url, "git@github.com:ewilloughby/BibApp.git"
